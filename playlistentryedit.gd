85574f76814584aa13e590a17b13f3b9d218f9bd
Lars Erik Grambo <0kvakk@gmail.com>
2023-02-07 15:01:39 +0100

    wip on new editor layout

--- playlistentryedit.gd
+++ playlistentryedit.gd
@@ -1,9 +1,11 @@
 extends HBoxContainer
 
 var _index:int
-var _path:String
+var _text:String
+var _showEdit:bool = false
 
 signal delete(index:int)
+signal edit(index:int)
 signal move(fromIndex:int, toIndex:int)
 
 func set_index(index:int):
@@ -12,9 +14,20 @@ func set_index(index:int):
 	%SpinBoxIndex.value = index + 1
 
 func set_path(path:String):
-	_path = path
+	_text = path
 	%Path.text = path.get_file().get_basename()
 	self.tooltip_text = path
+	setShowEdit(false)
+func set_name(name:String):
+	_text = name
+	%Path.text = name
+	self.tooltip_text = name
+	setShowEdit(true)
+
+func setShowEdit(val:bool):
+	_showEdit = val
+	%btnEdit.visible = val
+
 
 func _on_delete_pressed():
 	emit_signal("delete", _index)
