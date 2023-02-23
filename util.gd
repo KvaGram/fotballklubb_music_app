eca83a1abefb7beb9c994d4df7edf22e0a59e1e1
Lars Erik Grambo <0kvakk@gmail.com>
2023-02-23 14:34:32 +0100

    debugging wav

--- util.gd
+++ util.gd
@@ -10,9 +10,51 @@ func load_audio(path:String)->AudioStream:
 		stream.data = file.get_buffer(file.get_length())
 	elif path.get_extension() == "wav": #.ends_with(".wav"):
 		stream = AudioStreamWAV.new()
-		stream.stereo = true
-		stream.format = stream.FORMAT_16_BITS
-		stream.data = file.get_buffer(file.get_length())
+		
+		
+		#header data
+		var hd:int
+		var hds:String
+		
+		print("Parsing wav header...")
+		#parsing header
+		hds = ""
+		#hd=file.get_32() #expect ASCII literal RIFF. ignored
+		hds = get8bitCharStringFromfile(file, 4)
+		print(hds)
+		hd=file.get_32() #expect filesize. ignored
+		print("wav filesize: %d"%[hd])
+		#hd=file.get_32() #WAV description header in ASCII. ignored
+		hds = get8bitCharStringFromfile(file, 4)
+		print(hds)
+		hd=file.get_32() #chunk size. normaly 16
+		print("wav chunk size: %d"%[hd])
+		hd=file.get_16() #Wav type format. ???
+		print("wav format: %d"%[hd])
+		if hd >= 16: #This section is really a best guess. Only testing will tell if correct.
+			stream.format = stream.FORMAT_16_BITS
+		elif hd >= 8:
+			stream.format = AudioStreamWAV.FORMAT_8_BITS
+		else:
+			stream.format = AudioStreamWAV.FORMAT_IMA_ADPCM
+		hd=file.get_16() #number of audio channels print("wav byte rate: %d"%[hd])
+		print("wav channels: %d"%[hd])
+		stream.stereo = hd > 1
+		hd=file.get_32() #sample rate
+		print("wav sample rate: %d"%[hd])
+		hd=file.get_32() #byte rate
+		print("wav byte rate: %d"%[hd])
+		hd=file.get_16() #block allign
+		print("wav block align: %d"%[hd])
+		hd=file.get_16() #bytes per sample
+		print("wav bytes per sample: %d"%[hd])
+		#hd=file.get_32() #expect ASCII literal DATA. ignored
+		hds = get8bitCharStringFromfile(file, 4)
+		print(hds)
+		hd=file.get_32() #bytesize of data
+		print("wav data size: %d"%[hd])
+		print("Finished reading wav header")
+		stream.data = file.get_buffer(hd)
 	#elif path.get_extension() == "ogg": #.ends_with(".ogg"):
 	#	stream = AudioStreamOggVorbis.new()
 	#	stream.data = file.get_buffer(file.get_length())
@@ -23,3 +65,9 @@ func tosecminString(sec) -> String:
 	var s:int = int(sec) % 60
 	var m:int = int(sec) / 60
 	return "%02d:%02d" % [m, s]
+
+func get8bitCharStringFromfile(file:FileAccess, len:int) -> String:
+	var s:String = ""
+	for i in len:
+		s += char(file.get_8())
+	return s
